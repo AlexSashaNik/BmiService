565ea40e1999e5127509d292301c7767b8f18eda
AlexNikitina <alex.nikitina82@gmail.com>
2023-05-21 13:32:58 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+
+        int normalParameters = (int) service.calculate(65.00, 1.75);
+        System.out.println(normalParameters);
+
+    }
+
+}
+
+
+
